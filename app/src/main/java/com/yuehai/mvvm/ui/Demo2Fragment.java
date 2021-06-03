1e232dd24fc29597afc430528f2647186208cd1a
zhaoyuehai <zhaoyuehai5282@163.com>
2021-06-03 17:06:29 +0800

    mvvm

--- app/src/main/java/com/yuehai/mvvm/ui/Demo2Fragment.java
+++ app/src/main/java/com/yuehai/mvvm/ui/Demo2Fragment.java
@@ -1,24 +1,13 @@
 package com.yuehai.mvvm.ui;
 
-import android.graphics.Color;
-import android.os.Bundle;
-
 import com.yuehai.basic.BaseVMFragment;
 import com.yuehai.mvvm.BR;
 import com.yuehai.mvvm.R;
 import com.yuehai.mvvm.databinding.FragmentDemo2Binding;
 import com.yuehai.mvvm.vm.Demo2ViewModel;
 
-import org.jetbrains.annotations.Nullable;
-
 public class Demo2Fragment extends BaseVMFragment<FragmentDemo2Binding, Demo2ViewModel> {
     public Demo2Fragment() {
         super(R.layout.fragment_demo2, FragmentDemo2Binding::bind, BR.demo2VM, Demo2ViewModel.class);
-    }
-
-    @Override
-    protected void initView(@Nullable Bundle savedInstanceState) {
-        super.initView(savedInstanceState);
-        getBinding().demo2Tv.setTextColor(Color.BLUE);
     }
 }
